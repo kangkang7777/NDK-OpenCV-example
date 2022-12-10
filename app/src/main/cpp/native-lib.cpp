b47e0d6b211fa71bbdea39eb57a322d6b3a62ef0
KK <546628725@qq.com>
2022-12-10 18:46:19 +0800

    add keypoint

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,12 +1,16 @@
 #include <jni.h>
 #include <android/log.h>
+#include <opencv2/opencv.hpp>
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 
 #define TAG "NativeLib"
 
 using namespace std;
-using namespace cv;
+
+void orb(cv::Mat &mat);
+void fast(cv::Mat &mat);
+void sift(cv::Mat &mat);
 
 extern "C" {
 void JNICALL
@@ -15,15 +19,42 @@ Java_com_example_nativeopencvandroidtemplate_MainActivity_adaptiveThresholdFromJ
                                                                                    jlong matAddr) {
 
     // get Mat from raw address
-    Mat &mat = *(Mat *) matAddr;
+    cv::Mat &mat = *(cv::Mat *) matAddr;
 
     clock_t begin = clock();
 
-    cv::adaptiveThreshold(mat, mat, 255, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY_INV, 21, 5);
+    orb(mat);
+    fast(mat);
+    sift(mat);
 
     // log computation time to Android Logcat
     double totalTime = double(clock() - begin) / CLOCKS_PER_SEC;
     __android_log_print(ANDROID_LOG_INFO, TAG, "adaptiveThreshold computation time = %f seconds\n",
                         totalTime);
 }
+}
+
+void orb(cv::Mat &mat)
+{
+    cv::Ptr<cv::ORB> ptr = cv::ORB::create(3000);
+    vector<cv::KeyPoint> keyPoints;
+    ptr->detect(mat, keyPoints, cv::Mat());
+    drawKeypoints(mat, keyPoints, mat, cv::Scalar(0, 0, 255));
+}
+
+void fast(cv::Mat &mat)
+{
+    cv::Ptr<cv::FastFeatureDetector> ptr = cv::FastFeatureDetector::create(35);
+    vector<cv::KeyPoint> keyPoints;
+    ptr->detect(mat, keyPoints, cv::Mat());
+    drawKeypoints(mat, keyPoints, mat, cv::Scalar(255, 0, 0));
+}
+
+void sift(cv::Mat &mat){
+    cv::Mat grayImg;
+    cv::Ptr<cv::SIFT> ptr = cv::SIFT::create();
+    vector<cv::KeyPoint> keyPoints;
+    cvtColor(mat, grayImg, cv::COLOR_BGR2GRAY);
+    ptr->detect(grayImg, keyPoints, cv::Mat());
+    drawKeypoints(mat, keyPoints, mat, cv::Scalar(0, 255, 0),cv::DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS);
 }
